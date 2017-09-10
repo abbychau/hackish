8bb5f588a0bbe928ca12d94c04b815c10a346d05
Abby <abbychau@gmail.com>
2017-09-10 14:06:30 +0800

    Update .php
    fix template path

--- .php
+++ .php
@@ -1,16 +1,14 @@
 <?php
 //template - 3 lines
 function render($template,$vars){
-    echo preg_replace("/\{([^\{]{1,100}?)\}/e","$vars[$1]",file_get_contents("$template.tpl"));
+    echo preg_replace("/\{([^\{]{1,100}?)\}/e",'$vars[$1]',file_get_contents(__DIR__ ."/$template.tpl"));
 }
-
 //database 5 lines
 function dbAr($query){
     $tmp = mysqli_query($GLOBALS['conn'],$query) or die(mysqli_error($GLOBALS['conn']) . $query);
     for(;$arr[] = mysqli_fetch_assoc($tmp););
-    return array_pop($arr)==null?[]:$arr; //the last element from mysqli_fetch_assoc is supposed to be null
+    return array_pop($arr)==null?[]:$arr;
 }
-
 //auto router - 6 lines
 register_shutdown_function(function(){
     foreach ($GLOBALS['routes'] as $method =>$v) {
